b1ab2100e5039084804dfa08d7b0e183c20aaeb6
Geoffrey Arkenbout <razsiel@Geoffreys-Mac.local>
2017-10-09 12:10:11 +0200

    Initial Commit

---
+++ Newsreader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Newsreader
+//
+//  Created by Geoffrey Arkenbout on 10/9/17.
+//  Copyright © 2017 Geoffrey Arkenbout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
